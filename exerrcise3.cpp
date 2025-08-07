f7a9ce13c2c723f81384657d5be9d18016919b32
sovanchetrakeo-afk <sovanchetrakeo@gmail.com>
2025-08-07 12:01:53 +0700

    push

---
+++ exerrcise3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int firstNumber,secondNumber,result;
+    cout<<"Enter the first integer number:"<<"\n";
+    cin>>firstNumber;
+    cout<<"Enter the second integer number:"<<"\n";
+    cin>>secondNumber;
+    result=firstNumber+secondNumber;
+    cout<<"The first  integer number is "<<firstNumber<<"\n";
+    cout<<"The second integer number is "<<secondNumber<<"\n";
+    cout<<"The sum of  both   number is "<<result<<"\n";
+    return 0;
+}
